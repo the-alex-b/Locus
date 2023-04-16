650ad361e2f2bfbca2164e5ecf56bf4905de7536
Alex <46456279+the-alex-b@users.noreply.github.com>
2023-04-16 19:32:01 +0200

    rename

--- src/run.py
+++ src/run.py
@@ -1,4 +1,12 @@
-from local_vector_storage.index import Index
+from locus.index import Index, Vector
+import numpy as np
+from pprint import pprint
+
+index = Index(dimensions=10)
+
+embedding = np.float32(np.random.random((1, 10)))
+structured_data = {"a": 1}
+vector = Vector(embedding=embedding, data=structured_data)
 
 
-a = Index(dimensions=10)
+index.add_vector(vector)
